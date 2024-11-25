b15b6859235623fc29408dd4f25a0a4ddbf5809f
Renato <87155421+Renato5Lara@users.noreply.github.com>
2024-11-25 00:33:21 -0500

    Test Vocacional Recuperado

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { TestService } from './../src/app/page/services/test.service';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
